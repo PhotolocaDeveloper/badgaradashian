e58209867e68df2292ade84675fb0761f2928fcb
Andrew <Andreyd977@gmail.com>
2019-08-19 14:19:30 +0300

    Реализован класс NotificationBuilder для случая с уведомлением о истекшем сроке службы инвентаря

---
+++ functions/src/classses/notifications/InventoryEndsNotifBuilder.ts
@@ -0,0 +1,76 @@
+import {NotificationBuilder} from "../abstract/NotificationBuilder";
+import {NotificationClickAction} from "../../enums/NotificationClickAction";
+import {NotificationApnsCategory} from "../../enums/NotificationApnsCategory";
+import {AppEvent} from "../../enums/AppEvent";
+import * as admin from "firebase-admin";
+import {Inventory} from "../model/Inventory";
+import {NotificationMessage} from "../model/NotificationMessage";
+import {NotificationType} from "../../enums/NotificationType";
+import Firestore = admin.firestore;
+
+export class InventoryEndsNotifBuilder extends NotificationBuilder {
+
+    constructor(
+        private recipientId: string,
+        private relatedObject: Firestore.DocumentReference,
+        private inventory: Inventory
+    ) {
+        super();
+    }
+
+    public titleTemplate = "Проверь инвентарь";
+    public bodyTemplate = "Cрок службы инветнаря заканчивается {date_ends}";
+
+    buildRecipient(): void {
+        if (this.notification == null) return;
+        this.notification.recipient = this.recipientId;
+    }
+
+    buildType(): void {
+        if (this.notification == null) return;
+        this.notification.type = NotificationType.Personal
+    }
+
+    buildAndroidClickAction(): void {
+        if (this.notification == null) return;
+        this.notification.androidClickAction = NotificationClickAction.ShowInventory
+    }
+
+    buildApnsCategory(): void {
+        if (this.notification == null) return;
+        this.notification.apnsCategory = NotificationApnsCategory.InventoryEnds
+    }
+
+    buildEvent(): void {
+        if (this.notification == null) return;
+        this.notification.event = AppEvent.InventoryEnds
+    }
+
+    buildNotificationData(): void {
+        if (this.notification == null) return;
+        this.notification.notificationData = {
+            "inventory_id": this.relatedObject.id
+        }
+    }
+
+    buildNotificationMessage(): void {
+        if (this.notification == null) return;
+        let title = this.titleTemplate;
+        let body = this.bodyTemplate;
+        this.notification.notification = new NotificationMessage(title, body);
+    }
+
+    buildPlanedDateToDispatch(): void {
+        if (this.notification == null) return;
+        let dateToDispatch = this.inventory.nextReplacementDate.toMillis() - 3600 * 1000;
+        this.notification.plannedDateOfDispatch = Firestore.Timestamp.fromMillis(dateToDispatch);
+    }
+
+    buildRelatedObject(): void {
+        if (this.notification == null) return;
+        this.notification.relatedObject = this.relatedObject
+    }
+
+    buildResendingPeriod(): void {}
+
+}
