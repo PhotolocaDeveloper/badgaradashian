6b2ca09cc83dac212721e3490cf2a313f476e109
Andrew <Andreyd977@gmail.com>
2019-08-19 14:16:00 +0300

    Добавленны enum's с APNS category и Android click action

---
+++ functions/src/enums/NotificationApnsCategory.ts
@@ -0,0 +1,3 @@
+export enum NotificationApnsCategory {
+    InventoryEnds = "InventoryEndsCategory"
+}
---
+++ functions/src/enums/NotificationClickAction.ts
@@ -0,0 +1,3 @@
+export enum NotificationClickAction {
+    ShowInventory = "ru.badger.showInventoryAction"
+}
