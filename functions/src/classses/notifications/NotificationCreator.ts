f674ee45f8492e5248a98e5abc4880a8437ad385
Andrew <Andreyd977@gmail.com>
2019-08-19 14:20:00 +0300

    Создан класс NotificationCreator

---
+++ functions/src/classses/notifications/NotificationCreator.ts
@@ -0,0 +1,27 @@
+import {NotificationBuilder} from "../abstract/NotificationBuilder";
+import {NotificationPlanned} from "../model/NotificationPlanned"
+
+export class NotificationCreator {
+
+    constructor(
+        private notificationBuilder: NotificationBuilder
+    ) {}
+
+    public getNotification() : NotificationPlanned | null {
+        return this.notificationBuilder.notification;
+    }
+
+    public constructNotification() {
+        this.notificationBuilder.createNotification();
+        this.notificationBuilder.buildAndroidClickAction();
+        this.notificationBuilder.buildApnsCategory();
+        this.notificationBuilder.buildEvent();
+        this.notificationBuilder.buildNotificationData();
+        this.notificationBuilder.buildNotificationMessage();
+        this.notificationBuilder.buildPlanedDateToDispatch();
+        this.notificationBuilder.buildRecipient();
+        this.notificationBuilder.buildRelatedObject();
+        this.notificationBuilder.buildResendingPeriod();
+        this.notificationBuilder.buildType()
+    }
+}
