42f2ea872ab5e8dfe2dbc34c354aa3ce1a9f9b51
Andrew <Andreyd977@gmail.com>
2019-09-02 15:25:01 +0300

    Added Photo object

---
+++ functions/src/classses/model/Photo.ts
@@ -0,0 +1,8 @@
+import {JsonProperty, Serializable} from "typescript-json-serializer";
+import * as admin from "firebase-admin";
+
+@Serializable()
+export class Photo {
+    @JsonProperty() url?: string;
+    @JsonProperty('date_created') dateCreated?: admin.firestore.Timestamp;
+}
