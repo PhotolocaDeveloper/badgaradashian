77bab16fe3b7540f2dcb58a3259afe9441e7ab81
Andrew <Andreyd977@gmail.com>
2019-09-03 14:53:14 +0300

    Added photos deleting

--- functions/src/handlers/OnHousingDeleteHandler.ts
+++ functions/src/handlers/OnHousingDeleteHandler.ts
@@ -1,8 +1,10 @@
 import {DocumentSnapshot} from "firebase-functions/lib/providers/firestore";
 import {Functions} from "../firestore/Functions";
+import {EventContext} from "firebase-functions";
 
-export function onHousingDeleteHandler(snapshot: DocumentSnapshot) {
+export function onHousingDeleteHandler(snapshot: DocumentSnapshot, context: EventContext) {
     return Promise.all([
+        Functions.general().deleteRelatedPhotos(snapshot, context),
         Functions.housing().deleteRooms(snapshot)
     ])
 }
--- functions/src/handlers/OnRoomDeleteHandler.ts
+++ functions/src/handlers/OnRoomDeleteHandler.ts
@@ -1,8 +1,10 @@
 import {DocumentSnapshot} from "firebase-functions/lib/providers/firestore";
 import {Functions} from "../firestore/Functions";
+import {EventContext} from "firebase-functions";
 
-export function onRoomDeleteHandler(snapshot: DocumentSnapshot) {
+export function onRoomDeleteHandler(snapshot: DocumentSnapshot, context: EventContext) {
     return Promise.all([
+        Functions.general().deleteRelatedPhotos(snapshot, context),
         Functions.room().deleteInventoryLists(snapshot)
     ])
 }
