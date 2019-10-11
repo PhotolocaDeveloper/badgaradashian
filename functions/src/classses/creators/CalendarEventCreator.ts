b064c0349ddad8cb02c2c2452eeda67bc1744d63
Andrew <Andreyd977@gmail.com>
2019-10-11 14:35:35 +0300

    Added Calendar Event creator

---
+++ functions/src/classses/creators/CalendarEventCreator.ts
@@ -0,0 +1,23 @@
+import {CalendarEventBuilder} from "../builders/calendarEvents/CalendarEventBuilder";
+import {calendar_v3} from "googleapis";
+import Schema$Event = calendar_v3.Schema$Event;
+
+export class CalendarEventCreator {
+    constructor(private builder: CalendarEventBuilder) {
+    }
+
+    get(): Schema$Event {
+        return this.builder.event!
+    }
+
+    create(): CalendarEventCreator {
+        this.builder
+            .buildColorId()
+            .buildDescription()
+            .buildEnd()
+            .buildStart()
+            .buildSummary()
+            .build();
+        return this
+    }
+}
